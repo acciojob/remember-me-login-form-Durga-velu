2b16ffb09d1d13e18ac43a9d11fc9d9dcf03137f
Durga-velu <durgadevi134babu@gmail.com>
2025-04-22 11:43:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+
+let btn=document.getElementById("submit")
+let username=document.getElementById("username")
+let password=document.getElementById("password")
+let checkbox=document.getElementById("checkbox")
+let login=document.getElementById("existing")
+btn.addEventListener(("click"),(e)=>{
+
+	let userInfo=[];
+	userInfo.push({"username":"username.value", "password":"password.value"})
+	alert(`Logged in as ${username}`)
+	if(checkbox:checked){
+		
+	}
+	
+})
+
+
+
+
+
